12c04ac9dabf99ef6a1ae86987e6aaddc09dd248
Miguel Angel Fernandez <roiz@lobocom.es>
2025-05-26 13:08:52 +0200

    Reducimos localizaciones

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,12 +1,8 @@
 import { Location } from '../types';
 
 export const locations: Location[] = [
-  { id: '1', name: 'Madrid' },
-  { id: '2', name: 'Barcelona' },
+  { id: '1', name: 'Alicante' },
+  { id: '2', name: 'Murcia' },
   { id: '3', name: 'Valencia' },
-  { id: '4', name: 'Sevilla' },
-  { id: '5', name: 'Bilbao' },
-  { id: '6', name: 'Málaga' },
-  { id: '7', name: 'Zaragoza' },
   { id: '8', name: 'Todas' }
 ];
